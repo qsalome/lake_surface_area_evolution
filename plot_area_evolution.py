a71253d7099ced4c2a45698c48b179cff621619c
qsalome <qsalome.astro@gmail.com>
2025-08-19 19:08:57 +0300

    Update plot script to compare the different sampling

--- plot_area_evolution.py
+++ plot_area_evolution.py
@@ -1,4 +1,5 @@
 import pathlib
+import pandas as pd
 import geopandas
 import numpy as np
 import lmfit
@@ -10,32 +11,29 @@ from matplotlib.ticker import MultipleLocator
 
 
 #--------------------------------------------------------------------
-def convert_date2float(records_date):
+def convert_date2float(date_str):
    """
    Read the date as a string and convert it into a float
    that can be used to plot and fit the data.
    
    Parameters
    ----------
-   records_date: pandas.core.series.Series
-         string of the dates (%Y-%m-%d)
+   date_str: str
+         date in string format (%Y-%m-%d)
 
    Returns
    -------
    numpy.array of float
    """
-   datetimes  = [datetime.strptime(date,"%Y-%m-%d") for date in records_date]
-   timetuples = [date.timetuple() for date in datetimes]
+   date      = datetime.strptime(date_str,"%Y-%m-%d")
+   timetuple = date.timetuple()
 
-   dates = np.array([])
-   for date in timetuples:
-      if(calendar.isleap(date.tm_year)):
-         fdate = date.tm_year+(date.tm_yday-1)/366
-      else:
-         fdate = date.tm_year+(date.tm_yday-1)/365
-      dates = np.append(dates,fdate)
+   if(calendar.isleap(timetuple.tm_year)):
+      fdate = timetuple.tm_year+(timetuple.tm_yday-1)/366
+   else:
+      fdate = timetuple.tm_year+(timetuple.tm_yday-1)/365
 
-   return dates
+   return fdate
 
 #--------------------------------------------------------------------
 def fit_linear(y,x=None,err=None,method='leastsq'):
@@ -78,47 +76,84 @@ def fit_linear(y,x=None,err=None,method='leastsq'):
    return model
 
 #--------------------------------------------------------------------
-def plot_surface_area_evolution(dates,areas):
+def plot_surface_area_evolution(areas,lake_name=""):
    """
    Plot the evolution of the lake surface area.
    
    Parameters
    ----------
-   dates: list
-         month and year of the observations
-   area: pandas.core.series.Series
-         surface area of the lake in km2
+   areas: geopandas.geodataframe.GeoDataFrame
+         date, monthly, weekly and daily records of the surface area
+   lake_name: str
+         name of the lake
 
    Returns
    -------
    matplotlib.figure.Figure
          Evolution of the monthly surface area of the lake.
    """
+   #areas = areas.sort_values(by=['float_date'])
 
-   samp = np.where(areas>np.percentile(areas,5))
+   # Read the temperature for the selected city
+   dates   = np.array(areas["float_date"]).astype(float)
+   daily   = np.array(areas["area_daily"]).astype(float)
+   weekly  = np.array(areas["area_weekly"]).astype(float)
+   monthly = np.array(areas["area_monthly"]).astype(float)
 
-   y_min = np.percentile(areas,5)#np.min(areas)
-   y_max = np.max(areas)
+   y_min   = np.nanpercentile([daily,weekly,monthly],5)
+   y_max   = np.nanmax([daily,weekly,monthly])
 
-   ###########################
-   ### Add exponential fit ###
-   ###########################
+
 
    fig,ax = plt.subplots(figsize=(10,7))
 
-   ax.scatter(dates,areas, marker='o', s=8,color='black')
-   ax.plot(dates,areas, 'k-')
+   ### Plot monthly records
+   samp = np.where(np.isfinite(monthly))
+   if(len(samp[0]>0)):
+      ax.scatter(dates,monthly, marker='o', s=8,color='black')
+      ax.plot(dates,monthly, 'k-')
 
-   model = fit_linear(areas[samp],x=dates[samp])
-   area_evo = (model[-1]-model[0])/model[0]*100
-   ax.plot(dates[samp],model,'b--',
-               label=f'surface area: {area_evo:+.2f}%')
-   ax.fill_between(dates,0,np.percentile(areas,5),
-         color='xkcd:blue',alpha=0.2,linewidth=0)
+      samp = np.where((np.isfinite(monthly))&
+                      (monthly>np.nanpercentile(monthly,5))&
+                      (monthly<np.nanpercentile(monthly,95)))
+      model = fit_linear(monthly[samp],x=dates[samp])
+      area_evo = (model[-1]-model[0])/model[0]*100
+      ax.plot(dates[samp],model,'k--',
+                  label=f'surface area: {area_evo:+.2f}%')
+
+
+   ### Plot weekly records
+   samp = np.where(np.isfinite(weekly))
+   if(len(samp[0]>0)):
+      ax.scatter(dates,weekly, marker='o', s=8,color='red')
+      ax.plot(dates,weekly, 'r-')
+
+      samp = np.where((np.isfinite(weekly))&
+                      (weekly>np.nanpercentile(weekly,5))&
+                      (weekly<np.nanpercentile(weekly,95)))
+      model = fit_linear(weekly[samp],x=dates[samp])
+      area_evo = (model[-1]-model[0])/model[0]*100
+      ax.plot(dates[samp],model,'r--',
+                  label=f'surface area: {area_evo:+.2f}%')
+
+
+   ### Plot daily records
+   samp = np.where(np.isfinite(daily))
+   if(len(samp[0]>0)):
+      ax.scatter(dates,daily, marker='o', s=8,color='blue')
+      ax.plot(dates,daily, 'b-')
+
+      samp = np.where((np.isfinite(daily))&
+                      (daily>np.nanpercentile(daily,5))&
+                      (daily<np.nanpercentile(daily,95)))
+      model = fit_linear(daily[samp],x=dates[samp])
+      area_evo = (model[-1]-model[0])/model[0]*100
+      ax.plot(dates[samp],model,'b--',
+                  label=f'surface area: {area_evo:+.2f}%')
 
    ax.legend(loc='upper right')
 
-   plt.title(f"Lake surface area")
+   plt.title(f"Lake {lake_name} surface area")
    plt.xlabel("Date")
    plt.ylabel("Surface area (km$^2$)")
    plt.ylim([0.9*y_min,1.1*y_max])
@@ -142,22 +177,39 @@ DATA_DIRECTORY = NOTEBOOK_PATH / "data"
 FIG_DIRECTORY  = NOTEBOOK_PATH / "figures"
 
 
-daily_records   = geopandas.read_file(DATA_DIRECTORY / 
-                           "lake_patzcuaro_daily.gpkg")
-weekly_records  = geopandas.read_file(DATA_DIRECTORY /
-                           "lake_patzcuaro_weekly.gpkg")
-monthly_records = geopandas.read_file(DATA_DIRECTORY /
-                           "lake_patzcuaro_monthly.gpkg")
+areas = pd.DataFrame(columns=["date","float_date"]+[f"area_{sampling}"
+            for sampling in ["daily","weekly","monthly"]])
 
-k=0
-for records in [daily_records,weekly_records,monthly_records]:
-   areas = np.array(records.area)/1e6
-   dates = convert_date2float(records.date)
+lake_name="Patzcuaro"
 
-   fig,area = plot_surface_area_evolution(dates,areas)
-   #area_evo = np.append(area_evo,f"{area:+.2}")
-   fig.savefig(FIG_DIRECTORY / f"Surface_area_evolution_{k}.png")
+for sampling in ["weekly","monthly"]:
+   records   = geopandas.read_file(DATA_DIRECTORY / 
+                           f"{lake_name}_lake_evolution.gpkg",
+                           layer=f"{sampling}")
 
-   k = k+1
+   for i in range(len(records)):
+      entry = records.iloc[i]
+      d = {"date": entry['date'],
+           "float_date": convert_date2float(entry['date'])
+          }
+      if(sampling=="daily"):
+         d[f"area_daily"]   = [entry['geometry'].area/1e6]
+         d[f"area_weekly"]  = [np.nan]
+         d[f"area_monthly"] = [np.nan]
+      elif(sampling=="weekly"):
+         d[f"area_daily"]   = [np.nan]
+         d[f"area_weekly"]  = [entry['geometry'].area/1e6]
+         d[f"area_monthly"] = [np.nan]
+      elif(sampling=="monthly"):
+         d[f"area_daily"]   = [np.nan]
+         d[f"area_weekly"]  = [np.nan]
+         d[f"area_monthly"] = [entry['geometry'].area/1e6]
+
+      new_row = pd.DataFrame(d)
+      areas   = pd.concat([areas, new_row], ignore_index=True)
+
+
+fig,area = plot_surface_area_evolution(areas,lake_name=lake_name)
+fig.savefig(FIG_DIRECTORY / f"Surface_area_evolution_{lake_name}.png")
 
 
